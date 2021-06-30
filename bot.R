f986f6a7861e18f87ae55befbeda255256cf1728
Fabio Votta <fabio.votta@gmail.com>
2021-06-30 21:12:27 +0200

    error handle when there are no tweets

--- bot.R
+++ bot.R
@@ -35,14 +35,27 @@ ts_schwabs <- ts %>%
   mutate(link = stringr::str_extract(text, "http[^[:space:]]*"),
          schwabtext = stringr::str_replace(schwabtext, "hddb[^[:space:]]*", link))
 
-print("tweet it")
+ts_rows <- nrow(ts_schwabs) 
 
-ts_schwabs %>% 
+if(ts_rows==0){
+  
+    print("nothing to tweet")
+  
+} else {
+  print(paste0("tweet out", ts_rows, " tweets."))
+  
+ ts_schwabs %>% 
   split(1:nrow(.)) %>% 
   purrr::walk(~{
+    
+    print(.x$schwabtext)
     
     Sys.sleep(5)
     
     twitteR::tweet(text = .x$schwabtext, inReplyTo = .x$id, bypassCharLimit = T)
     
   })
+}
+
+
+
